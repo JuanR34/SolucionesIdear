2450a764553357c3dd30f8d0133e170b72b00725
JuanR34 <juanpablo_jpr@hotmail.com>
2022-08-22 01:22:50 -0300

    chart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React from "react";
 import Logo from './components/Logo';
 import Navbar from './components/Navbar';
+import LineChart from './components/LineChart';
 //import Cabezal from './img/Cabezal.jpg'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
        {/* <img src={Cabezal} alt="henry-movies-logo"></img> */}
     <Logo/>
     <Navbar/>
+    <LineChart/>
     </div>
   );
 }
